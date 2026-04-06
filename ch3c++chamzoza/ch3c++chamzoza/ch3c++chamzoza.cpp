c8f20579283c1faeb2f98d9f0f1732abbb6b87ff
Agaramji <asd2580506@gmail.com>
2026-04-06 21:11:33 +0900

    Add files via upload

---
+++ ch3c++chamzoza/ch3c++chamzoza/ch3c++chamzoza.cpp
@@ -0,0 +1,29 @@
+﻿// ch3c++chamzoza.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int change_val(int* p) {
+	*p = 3;
+
+	return 0;
+
+}
+int main() {
+	int number = 5;
+
+	std::cout << number << std::endl;
+	change_val(&number);
+	std::cout << number << std::endl;
+	std::cout << number << std::endl;
+
+	int a = 3;
+	int& another_a = a;
+
+	another_a = 5;
+	std::cout << "a : " << a << std::endl;
+	std::cout << "another_a : " << another_a << std::endl;
+
+	return 0;
+
+}
---
+++ mainc++/mainc++/mainc++.cpp
@@ -0,0 +1,43 @@
+﻿// mainc++.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+
+int main() {
+
+	int user_input;
+	cout << "내정보" << endl;
+	cout << "1.이름" << endl;
+	cout << "2.나이" << endl;
+	cout << "3.성별" << endl;
+	cin >> user_input;
+
+	switch (user_input) {
+
+	case 1:
+		cout << "이지환" << endl;
+		break;
+
+	case 2:
+		cout << "26" << endl;
+		break;
+
+	case 3:
+		cout << "남" << endl;
+		break;
+
+	default:
+		cout << "끝" << endl;
+		break;
+
+	}
+
+	return 0;
+	
+	
+}
+
